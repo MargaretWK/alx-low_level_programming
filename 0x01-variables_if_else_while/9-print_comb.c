a1c9d76dc9559f9f206e5a17838f2ff4be8e552f
MargaretWK <magiwambui9@gmail.com>
2023-04-03 15:20:19 +0300

    prints all possible combinations of single digit numbers,in ascending order, separated by comma, followed by a space using putchar function

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+* Return: always 0
+*/
+int main(void)
+{
+int d;
+
+for (d = 0; d < 10; d++)
+{
+	putchar(d + '0');
+	if (d != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
